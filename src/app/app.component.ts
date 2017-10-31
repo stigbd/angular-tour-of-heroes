283231cce394803a453592caed520ed1d7b9011a
Stig B. Dørmænen <stigbd@gmail.com>
2017-10-31 12:15:11 +0100

    Routing implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  styleUrls: [ './app.component.css' ],
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `
 })
 
