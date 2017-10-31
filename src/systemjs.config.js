3e42caa9e9aee133fbcd2ce9ee17a1a4aa59bea7
Stig B. Dørmænen <stigbd@gmail.com>
2017-10-31 15:05:35 +0100

    Working first version of http in-memory-web-api

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -22,8 +22,10 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
 
       // other libraries
+      'tslib': 'npm:tslib/tslib.js',
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
