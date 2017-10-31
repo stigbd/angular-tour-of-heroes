6cd097ec78ae8b4ab68d739c3f5518e5dcd57454
Stig B. Dørmænen <stigbd@gmail.com>
2017-10-31 15:19:54 +0100

    Add update

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,7 @@ import { Hero } from './hero';
 export class HeroService {
 
   private heroesUrl = 'api/heroes'; // URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) { }
 
@@ -19,20 +20,25 @@ export class HeroService {
     .catch(this.handleError);
   }
 
+  getHero(id: number): Promise<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+    .toPromise()
+    .then(response => response.json() as Hero)
+    .catch(this.handleError)
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+    .put(url, JSON.stringify(hero), {headers: this.headers})
+    .toPromise()
+    .then(() => hero)
+    .catch(this.handleError);
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occured', error) //for demo purposes only
     return Promise.reject(error.message || error);
-  }
-
-  getHeroesSlowly(): Promise<Hero[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(this.getHeroes()), 2000);
-    });
-  }
-
-  getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-    .then(heroes => heroes.find(hero => hero.id === id));
   }
 }
